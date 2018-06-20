0b29abc7c5c7a7095eda58bccb980539463d0f08
mthh <matthieu.viry@cnrs.fr>
2018-06-20 23:34:22 +0200

    ...

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 [Présentation de Magrit au "midi du Pôle Image" (Paris - 21 mars 2017)](http://mthh.github.io/carto/)  
 [Présentation de Magrit à l'ICC / Conférence Internationnale de Cartographie (Washington DC - 3 juillet 2017)](http://mthh.github.io/icc17/)  
 [Magrit - Session "trucs et astuces" au "midi du Pôle Image" (Paris - 29 mars 2018)](http://mthh.github.io/poleimage_magrit_180329/)  
-
+["Mettre en ligne une carte sans Google (session 2 - avec Violaine Jurie)" au "midi du Pôle Image" (Paris - 21 juin 2018)](http://mthh.github.io/poleimage_osm_180621/)  
---
+++ poleimage_osm_180621/qgis2web_paris2/data/bat_commune_wikidata_4.js
@@ -0,0 +1,16 @@
+var json_bat_commune_wikidata_4 = {
+"type": "FeatureCollection",
+"name": "bat_commune_wikidata_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "itemLabel": "colonne Vendôme", "edgeLabel": "démolition", "pic2": "<img src=\"images\/Vendome_Column_A.jpg\" width=\"180px\">" }, "geometry": { "type": "Point", "coordinates": [ 2.329414, 48.867469 ] } },
+{ "type": "Feature", "properties": { "itemLabel": "Fondation Dosne-Thiers", "edgeLabel": "démolition", "pic2": "<img src=\"images\/Hotel_Thiers_1846.jpg\" width=\"180px\">" }, "geometry": { "type": "Point", "coordinates": [ 2.3375, 48.87861111 ] } },
+{ "type": "Feature", "properties": { "itemLabel": "palais de justice de Paris", "edgeLabel": "incendie", "pic2": "<img src=\"images\/Palais_de_Justice.jpg\" width=\"180px\">" }, "geometry": { "type": "Point", "coordinates": [ 2.345051, 48.855722 ] } },
+{ "type": "Feature", "properties": { "itemLabel": "palais des Tuileries", "edgeLabel": "incendie", "pic2": "<img src=\"images\/Tuileries_vers_1860.jpg\" width=\"180px\">" }, "geometry": { "type": "Point", "coordinates": [ 2.3325, 48.862222222 ] } },
+{ "type": "Feature", "properties": { "itemLabel": "hôtel de ville de Paris", "edgeLabel": "incendie", "pic2": "<img src=\"images\/Hotel_de_Ville_Paris.jpg\" width=\"180px\">" }, "geometry": { "type": "Point", "coordinates": [ 2.352222, 48.856389 ] } },
+{ "type": "Feature", "properties": { "itemLabel": "manufacture des Gobelins", "edgeLabel": "incendie", "pic2": "<img src=\"images\/Manufacture_des_Gobelins.jpg\" width=\"180px\">" }, "geometry": { "type": "Point", "coordinates": [ 2.35312, 48.83495 ] } },
+{ "type": "Feature", "properties": { "itemLabel": "palais de la Légion d'honneur", "edgeLabel": "incendie", "pic2": "<img src=\"images\/Hotel-de-Salm-Paris.JPG\" width=\"180px\">" }, "geometry": { "type": "Point", "coordinates": [ 2.324444, 48.860361 ] } },
+{ "type": "Feature", "properties": { "itemLabel": "palais d'Orsay", "edgeLabel": "incendie", "pic2": "<img src=\"images\/Palais_dOrsay.gif\" width=\"180px\">" }, "geometry": { "type": "Point", "coordinates": [ 2.3269444, 48.86 ] } },
+{ "type": "Feature", "properties": { "itemLabel": "théâtre de la Porte-Saint-Martin", "edgeLabel": "démolition", "pic2": "<img src=\"images\/Theatre_de_la_Porte_Saint-Martin.JPG\" width=\"180px\">" }, "geometry": { "type": "Point", "coordinates": [ 2.356666666, 48.869027777 ] } }
+]
+}
