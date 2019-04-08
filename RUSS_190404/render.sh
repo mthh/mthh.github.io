ff508ee81f9fe29640a8b1445ec790aaa967be06
mthh <matthieu.viry@cnrs.fr>
2019-04-08 15:47:16 +0200

    ...

--- RUSS_190404/render.sh
+++ RUSS_190404/render.sh
@@ -6,4 +6,12 @@ jupyter-nbconvert --to slides presentationRuss.ipynb --SlidesExporter.reveal_scr
 && jupyter-nbconvert --to slides ManipulationGeospatiale.ipynb --SlidesExporter.reveal_scroll=True \
 && jupyter-nbconvert --to slides web.ipynb --SlidesExporter.reveal_scroll=True \
 && jupyter-nbconvert --to slides RetourDiscussion.ipynb --SlidesExporter.reveal_scroll=True \
-&& mv presentationRuss.slides.html index.html
+&& mv presentationRuss.slides.html index.html \
+&& rm -rf ~/code/mthh.github.io/RUSS_190404/* \
+&& rm -rf ~/code/mthh.github.io/binder/* \
+&& cp -r . ~/code/mthh.github.io/RUSS_190404/ \
+&& rm ~/code/mthh.github.io/RUSS_190404/RustKernel.ipynb \
+&& mv ~/code/mthh.github.io/RUSS_190404/requirements.txt ~/code/mthh.github.io/binder/ \
+&& mv ~/code/mthh.github.io/RUSS_190404/runtime.txt ~/code/mthh.github.io/binder/
+
+
