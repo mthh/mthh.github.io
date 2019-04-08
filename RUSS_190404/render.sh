add129279e8b5d741ac34e9b70b94a0edc6a9ff1
mthh <matthieu.viry@cnrs.fr>
2019-04-08 13:18:08 +0200

    ...

--- RUSS_190404/render.sh
+++ RUSS_190404/render.sh
@@ -3,6 +3,7 @@ jupyter-nbconvert --to slides presentationRuss.ipynb --SlidesExporter.reveal_scr
 && jupyter-nbconvert --to slides CalculVisu.ipynb --SlidesExporter.reveal_scroll=True \
 && jupyter-nbconvert --to slides Pydata.ipynb --SlidesExporter.reveal_scroll=True \
 && jupyter-nbconvert --to slides StatsML.ipynb --SlidesExporter.reveal_scroll=True \
+&& jupyter-nbconvert --to slides ManipulationGeospatiale.ipynb --SlidesExporter.reveal_scroll=True \
 && jupyter-nbconvert --to slides web.ipynb --SlidesExporter.reveal_scroll=True \
 && jupyter-nbconvert --to slides RetourDiscussion.ipynb --SlidesExporter.reveal_scroll=True \
 && mv presentationRuss.slides.html index.html
